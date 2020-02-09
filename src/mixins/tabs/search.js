c76a37a7a076362966b6b7a34306679c9e88d1bf
Marshu668 <869715017@qq.com>
2020-02-09 15:40:48 +0800

    历史搜素的关键词的存储显示和隐藏

--- src/mixins/tabs/search.js
+++ src/mixins/tabs/search.js
@@ -5,7 +5,15 @@ export default class extends wepy.mixin {
     //   搜索框的默认内容
     value: '',
     // 搜索建议列表
-    sugguestList: []
+    sugguestList: [],
+    // 搜索历史列表
+    kwList: []
+  }
+  onLoad() {
+    //   页面加载就获取到相应到搜索列表，不存在就返回空数组，存在就返回给kwList
+    const kwList = wx.getStorageSync('kw') || []
+    this.kwList = kwList
+    console.log(kwList)
   }
   methods = {
     //   当搜索关键词发送变化，就好触发这个事件处理函数
@@ -21,7 +29,21 @@ export default class extends wepy.mixin {
     //   触发了搜索
     onSearch(e) {
       // e.detail 就是最新 的搜索关键词
+      const kw = e.detail.trim()
       console.log(e.detail)
+      if (kw.length <= 0) {
+        return
+      }
+      //   把用户填写到搜索关键词，保存到storage中
+      if (this.kwList.indexOf(kw) === -1) {
+        this.kwList.unshift(kw)
+      }
+      //   数组的slice方法不会修改愿数组，而是返回一个新数组
+      this.kwList = this.kwList.slice(0, 10)
+      wepy.setStorageSync('kw', this.kwList)
+      wepy.navigateTo({
+        url: '/pages/goods_list?query=' + kw
+      })
     },
     // 触发了取消
     onCancel() {
